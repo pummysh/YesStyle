c8abeb9210b4c788e712f73235e3c00fa601666d
rajesh4210 <75352443+rajesh4210@users.noreply.github.com>
2021-09-28 19:18:39 +0530

    Navbar functionality

---
+++ homepage.js
@@ -0,0 +1,9 @@
+var newInDropDown = document.getElementById("NewInPara");
+
+newInDropDown.addEventListener("click",newInDropDownHover);
+
+
+function newInDropDownHover(el){
+    contentBox = document.getElementById("dropDownNewIn");
+}
+
